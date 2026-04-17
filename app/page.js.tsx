5f312c8b775b4237774875d3fbe953512986e0cc
AhmedAlsayegh <ahmed90013@gmail.com>
2026-04-17 23:53:18 +0300

    fix api route location

---
+++ app/page.js.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl text-blue-600 font-bold">
+        نظام إدارة العقارات
+      </h1>
+    </div>
+  );
+}
+
